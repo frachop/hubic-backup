2805a99572e505f83720964861c8cc98a82f1b3d
franckc <franck.chopin@h-t-solutions.com>
2015-03-19 18:30:14 +0100

    atomic

--- src/asset.h
+++ src/asset.h
@@ -1,6 +1,6 @@
 /*************************************************************************/
 /* hubic-backup - an fast and easy to use hubic backup CLI tool          */
-/* Copyright (c) 2015 Franck Chopin.                                     */
+/* Copyright (c) 2014 Gabi Melman.                                       */
 /*                                                                       */
 /* Permission is hereby granted, free of charge, to any person obtaining */
 /* a copy of this software and associated documentation files (the       */
@@ -26,6 +26,7 @@
 
 #include <vector>
 #include <mutex>
+#include <atomic> 
 #include <boost/filesystem.hpp>
 #include "md5.h"
 
